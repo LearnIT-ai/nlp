7a321956f681e975944046e748fe13b7c2e86668
MaksKG <144100021+MaksKG@users.noreply.github.com>
2025-05-31 15:46:00 +0300

    Update routeRegistrator.py

--- server/routeRegistrator.py
+++ server/routeRegistrator.py
@@ -1,9 +1,10 @@
 from AI_router import ai_access
-
+from default_router import default_router
 class RouteRegistrator:
     def __init__(self, app):
         self.app = app
 
     def register_all(self):
         self.app.include_router(ai_access)
+        self.app.include_router(default_router)
         return self.app
